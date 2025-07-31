e8906080e1073c1a1431e9953364babb25990904
bonaparks-dev <bonaparks@gmail.com>
2025-07-31 14:44:32 +0200

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,7 +9,7 @@ export function Header() {
 
   return (
     <>
-      <header className="fixed top-0 left-0 right-0 z-50 bg-gradient-to-b from-black/90 to-black/0 backdrop-blur-md">
+      <header className="fixed top-0 left-0 right-0 z-50 bg-[linear-gradient(to_bottom,_rgba(0,0,0,0.9),_rgba(0,0,0,0))] backdrop-blur-md">
         <div className="container mx-auto px-4 py-2 flex flex-col items-center relative">
 
           {/* Boutons à droite positionnés un peu plus bas */}
