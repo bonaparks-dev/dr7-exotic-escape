18d027892c577f9fd5b0689b47226ad4fd5349be
bonaparks-dev <bonaparks@gmail.com>
2025-07-31 14:36:34 +0200

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,9 @@ export default {
 			}
 		},
 		extend: {
+			fontFamily: {
+				seasons: ['"The Seasons"', 'serif'],
+			},
 			colors: {
 				border: 'hsl(var(--border))',
 				input: 'hsl(var(--input))',
