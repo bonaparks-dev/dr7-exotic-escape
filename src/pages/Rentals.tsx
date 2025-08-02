cf8dd9964cfeef58f01846b452f947a4c20000b7
bonaparks-dev <bonaparks@gmail.com>
2025-08-02 17:12:44 +0200

    Update Rentals.tsx

--- src/pages/Rentals.tsx
+++ src/pages/Rentals.tsx
@@ -36,7 +36,7 @@ const cars: Car[] = [
       engine: "3.0L inline 6-cylinder"
     },
     description: "Un'icona che domina la strada.",
-    image: "/images/cars/bmw-m3.jpg"
+    image: "/bmw-m3.png"
   },
   {
     id: 2,
