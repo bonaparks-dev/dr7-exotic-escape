9d9457ff18974e7b21adfe5934ab35fecc7f79b8
bonaparks-dev <bonaparks@gmail.com>
2025-08-07 15:27:17 +0200

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -12,7 +12,7 @@ export function Footer() {
           {/* Brand */}
           <div className="space-y-4">
             <img 
-              src="/logo1.png" 
+              src="/logo2.png" 
               alt="DR7 Exotic Cars & Luxury" 
               className="h-12 w-auto"
             />
